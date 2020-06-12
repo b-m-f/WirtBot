437b502bc5daa90e60c44b01e4a9d5e335931130
b-m-f <max@ehlers.berlin>
2020-06-12 17:07:10 +0200

    Update e2e tests to run in CI. Add docker and docker-compose setup for running all tests in CI

--- Interface/tests/helpers/dataFillers.js
+++ Interface/tests/helpers/dataFillers.js
@@ -7,32 +7,32 @@ async function setServer(browser) {
 
   await browser.click("#server-widget #edit");
   await browser.setValue("#server-widget input[name='1']", "192");
-  await browser.execute(function() {
+  await browser.execute(function () {
     var element = document.querySelector("#server-widget input[name='1']");
     var event = new Event("change");
     element.dispatchEvent(event);
   });
   await browser.setValue("#server-widget input[name='2']", "168");
-  await browser.execute(function() {
+  await browser.execute(function () {
     var element = document.querySelector("#server-widget input[name='2']");
     var event = new Event("change");
     element.dispatchEvent(event);
   });
   await browser.setValue("#server-widget input[name='3']", "0");
-  await browser.execute(function() {
+  await browser.execute(function () {
     var element = document.querySelector("#server-widget input[name='3']");
     var event = new Event("change");
     element.dispatchEvent(event);
   });
   await browser.setValue("#server-widget input[name='4']", "2");
-  await browser.execute(function() {
+  await browser.execute(function () {
     var element = document.querySelector("#server-widget input[name='4']");
     var event = new Event("change");
     element.dispatchEvent(event);
   });
 
   await browser.setValue("#server-widget input[name='port']", "1233");
-  await browser.execute(function() {
+  await browser.execute(function () {
     var element = document.querySelector("#server-widget input[name='port']");
     var event = new Event("change");
     element.dispatchEvent(event);
