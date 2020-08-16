2352c61992b3409d520fa6242f6c8e88b330e21e
b-m-f <max@ehlers.berlin>
2020-08-16 14:21:44 +0200

    Fix access to internal zone name

--- Interface/src/lib/dns.js
+++ Interface/src/lib/dns.js
@@ -2,29 +2,29 @@
 export function generateDNSFile(server, clients, network) {
     const deviceNames = clients.map(client => {
         if (client.ip.v6 && client.ip.v4) {
-            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dnsName}
-        ${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dnsName}`
+            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dns.name}
+        ${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dns.name}`
         }
         if (client.ip.v6 && !client.ip.v4) {
-            return `${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dnsName}`
+            return `${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dns.name}`
         }
         if (!client.ip.v6 && client.ip.v4) {
-            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dnsName}`
+            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dns.name}`
         }
     })
     const serverName = () => {
         if (server.subnet.v6 && server.subnet.v4) {
-            return `${server.subnet.v4 + '1'} wirtbot.${network.dnsName}
-        ${server.subnet.v6 + '1'} wirtbot.${network.dnsName}`
+            return `${server.subnet.v4 + '1'} wirtbot.${network.dns.name}
+        ${server.subnet.v6 + '1'} wirtbot.${network.dns.name}`
         }
         if (server.subnet.v6 && !server.subnet.v4) {
-            return `${server.subnet.v6 + "1"} wirtbot.${network.dnsName}`
+            return `${server.subnet.v6 + "1"} wirtbot.${network.dns.name}`
         }
         if (!server.subnet.v6 && server.subnet.v4) {
-            return `${server.subnet.v4 + "1"} wirtbot.${network.dnsName}`
+            return `${server.subnet.v4 + "1"} wirtbot.${network.dns.name}`
         }
     }
-    const masterFile = `${network.dnsName} {
+    const masterFile = `${network.dns.name} {
     reload
     hosts {
         ${serverName()}
