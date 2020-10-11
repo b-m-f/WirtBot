80e6c7460d7f57003785a6cb8bc014d31fa926de
b-m-f <max@ehlers.berlin>
2020-10-11 19:07:19 +0200

    Fix build after including wasm module. Webpack messes up the require since it is not known at bundle time, what the WASM module is going to require
    See https://github.com/rustwasm/wasm-bindgen/issues/2323

--- installer/webpack.config.js
+++ installer/webpack.config.js
@@ -4,8 +4,12 @@ module.exports = {
 	entry: './index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'installer.js'
+		filename: 'installer.js',
+		chunkFormat: 'commonjs'
 	},
-	target: 'node',
+	target: 'async-node',
+	optimization: {
+		minimize: false,
+	},
 	experiments: { syncWebAssembly: true },
 };
