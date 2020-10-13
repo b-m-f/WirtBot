baa9b6befc7b52b764a04a2b57486709e2164060
b-m-f <max@ehlers.berlin>
2020-10-13 19:55:40 +0200

    Finish setting up a hidden WirtBot with Interface

--- installer/index.js
+++ installer/index.js
@@ -9,6 +9,7 @@ const configPath = "./wirt-installer.config.json"
 
 const config = new Configstore("wirt-installer", {}, { configPath });
 
+
 const runAnsible = async ({
     user,
     serverIP,
@@ -148,24 +149,20 @@ const main = async () => {
                 if (entry !== 'password') {
                     config.set(entry, response[entry])
                 }
-            })
+            });
 
             const serverKeys = await getKeys();
             const deviceKeys = await getKeys();
-            const signingKeys = await generateSigningKeys()
-            const device = { ip: { v4: 2 }, name: "Change me", keys: deviceKeys }
+            const signingKeys = await generateSigningKeys();
+            const device = { ip: { v4: 2 }, name: "Change me", keys: deviceKeys };
             // TODO: Fix IPv4 to always be a string 
             // needs changes in many places
-            const server = { ip: { v4: config.get('serverIP').split('.') }, port: 10101, keys: serverKeys, subnet: { v4: "10.10.0." } }
-            const serverConfig = generateServerConfig(server, [device])
-            const deviceConfig = generateDeviceConfig(device, server)
-            const dnsConfig = generateDNSFile(server, [device], { dns: { name: "wirt.internal" } })
+            const server = { ip: { v4: config.get('serverIP').split('.') }, port: 10101, keys: serverKeys, subnet: { v4: "10.10.0." } };
+            const serverConfig = generateServerConfig(server, [device]);
+            const deviceConfig = generateDeviceConfig(device, server);
+            const dnsConfig = generateDNSFile(server, [device], { dns: { name: "wirt.internal" } });
 
-            runAnsible(Object.assign({}, config.all, { password: response.password, wirtBotUIKey: signingKeys.public, update: false, serverConfig, dnsConfig }))
-            console.log("DONE")
-            console.log(deviceConfig)
-            console.log(serverConfig)
-            console.log({
+            const interfaceState = JSON.stringify({
                 state: {
                     // TODO keep this version somewhere else
                     version: 1.1,
@@ -173,21 +170,29 @@ const main = async () => {
                     devices: [device],
                     keys: signingKeys
                 }
-            })
+            });
+
+            runAnsible(Object.assign({}, config.all, { password: response.password, wirtBotUIKey: signingKeys.public, update: false, serverConfig, dnsConfig }));
+            console.log("DONE");
+            console.log(deviceConfig);
+            console.log(serverConfig);
+            console.log(interfaceState);
+            await fs.writeFile('ImportThisFileIntoYourWirtBotInterface.json', interfaceState, 'utf8');
+
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
     }
     if (updateOrInstall["value"] === 'update') {
         const response = await prompts(questionsUpdate);
-        console.log("Configuration written to", configPath)
+        console.log("Configuration written to", configPath);
         Object.keys(response).forEach(entry => {
             if (entry !== 'password') {
                 config.set(entry, response[entry])
             }
         })
         // TODO: Make this configurable in case the subnet is changed
-        runAnsible(Object.assign({}, config.all, { server: { ip: { v4: "10.10.0.1" } }, password: response.password, update: true, sshPrivateKeyPath: response.sshPrivateKeyPath }))
+        runAnsible(Object.assign({}, config.all, { server: { ip: { v4: "10.10.0.1" } }, password: response.password, update: true, sshPrivateKeyPath: response.sshPrivateKeyPath }));
     }
 
 
