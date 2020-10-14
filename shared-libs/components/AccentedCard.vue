73d11f165ed8612ab41fc8bea8d24682e0c204be
b-m-f <max@ehlers.berlin>
2020-10-14 17:37:01 +0200

    Add more shared-libs and a sass loader for the docs

---
+++ shared-libs/components/AccentedCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <Card id="card" :class="{ blue: type === 'info', padded: padded }">
+    <slot></slot
+  ></Card>
+</template>
+
+<script>
+import Card from "./Card";
+export default {
+  components: { Card },
+  props: { type: { type: String, required: true }, padded: Boolean },
+};
+</script>
+
+<style lang="scss" scoped>
+#card {
+  &.blue {
+    border-left: $border-large solid $primary;
+  }
+  &.padded {
+    padding: $spacing-large;
+  }
+}
+</style>
