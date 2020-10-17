8af967fd24523b9177e5b1872434415be6b4d470
b-m-f <max@ehlers.berlin>
2020-10-17 12:43:37 +0200

    Extracts system test into own module

--- System-Tests/nightwatch.conf.js
+++ System-Tests/nightwatch.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ["tests/e2e"],
-  exclude: ["tests/e2e/helpers"],
+  src_folders: ["tests/nightwatch"],
+  exclude: ["tests/nightwatch/helpers"],
 
   test_settings: {
     default: {
