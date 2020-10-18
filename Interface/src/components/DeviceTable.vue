5893cce09c2cd0450c00f6b126182d24888899af
b-m-f <max@ehlers.berlin>
2020-10-18 12:31:53 +0200

    Generates IDs for devices that were added manually into a backup file and marks them as such

--- Interface/src/components/DeviceTable.vue
+++ Interface/src/components/DeviceTable.vue
@@ -47,6 +47,8 @@
 
 <script>
 import DeviceRow from "./DeviceRow";
+import { guidGenerator } from "../lib/helpers";
+
 export default {
   components: { DeviceRow },
   props: { devices: Array, expanded: Boolean },
@@ -113,28 +115,6 @@ export default {
           additionalDNSServers,
           MTU,
         });
-      }
-      // https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id/6860916#6860916
-      function guidGenerator() {
-        var S4 = function () {
-          return (((1 + Math.random()) * 0x10000) | 0)
-            .toString(16)
-            .substring(1);
-        };
-        return (
-          S4() +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          S4() +
-          S4()
-        );
       }
 
       const newId = guidGenerator();
