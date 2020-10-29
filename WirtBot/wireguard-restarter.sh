863158eeafe56dfc18dc7d7528de2f45ee87cc6b
b-m-f <max@ehlers.berlin>
2020-10-29 23:01:48 +0100

    Use wg-quick in restarter in a way that will not disrupt current connections

--- WirtBot/wireguard-restarter.sh
+++ WirtBot/wireguard-restarter.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-while inotifywait -e close_write /etc/wireguard/server.conf; do systemctl restart wg-quick@server; done
+while inotifywait -e close_write /etc/wireguard/server.conf; do wg syncconf server <(wg-quick strip server); done
