f9328f0e4fb32431627afd5041f10f30fd0e0efd
b-m-f <max@ehlers.berlin>
2020-11-04 21:39:03 +0100

    Update to 1.4.1

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -31,7 +31,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: { alerts },
   state: {
-    version: "1.4.0",
+    version: "1.4.1",
     keys: undefined,
     server: {
       ip: { v4: [undefined, undefined, undefined, undefined], v6: "" },
