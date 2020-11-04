e80fbb4207409167597dd3acccb63ab1aed0652c
b-m-f <max@ehlers.berlin>
2020-11-04 19:29:06 +0100

    Fix store version update and correctly display no server error message

--- update_version.sh
+++ update_version.sh
@@ -3,7 +3,7 @@ filename='.version'
 while read line; do
 	echo "Updating all packages to $line"
 	find . -type f -name "package.json" -not -path "*/node_modules**" -exec sed -i -e "s|version\": \".*\"|version\": \"$line\"|" {} \;
-	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|version: .*,|version: $line,|" {} \;
+	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|version: \".*\",|version: \"$line\",|" {} \;
 	find . -type f -name "Cargo.toml" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
 	echo "Finished updating please release the installer if necessary and create a Tag to release"
 done < $filename
