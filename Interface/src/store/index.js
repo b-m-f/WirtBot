b95e1a045d2e8f7632c5ae879fd447d67a3a60c4
b-m-f <max@ehlers.berlin>
2020-11-13 21:56:26 +0100

    Bump to 1.4.5

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -31,7 +31,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: { alerts },
   state: {
-    version: "1.4.4",
+    version: "1.4.5",
     keys: undefined,
     server: {
       ip: { v4: [undefined, undefined, undefined, undefined], v6: "" },
