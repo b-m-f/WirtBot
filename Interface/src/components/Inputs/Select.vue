4f457d03e0764525830d6ee5cb756eec140ff882
b-m-f <max@ehlers.berlin>
2020-11-14 10:40:50 +0100

    Create select component and use on devies

---
+++ Interface/src/components/Inputs/Select.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <select
+    :name="$props.name"
+    ref="select"
+    :required="$props.required"
+    @change="update"
+    :value="$props.selected"
+  >
+    <option
+      :value="option"
+      v-for="(option, index) in $props.options"
+      :key="index"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    options: Array,
+    required: Boolean,
+    selected: [String, Number, Object],
+  },
+  methods: {
+    update(event) {
+      console.log(event.target.value);
+      this.$emit("change", event.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
