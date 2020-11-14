164a2dddf3383ec35f7e392dd5eaf89bc5bfc41a
b-m-f <max@ehlers.berlin>
2020-11-14 10:23:46 +0100

    Create and use checkbox component

---
+++ Interface/src/components/Inputs/CheckBox.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <input
+    type="checkbox"
+    :name="$props.name"
+    :checked="$props.checked"
+    @change="(e) => update(e.target.checked)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    checked: Boolean,
+    name: String,
+  },
+  methods: {
+    update(bool) {
+      this.$emit("change", bool);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- Interface/src/components/Inputs/Text.vue
+++ Interface/src/components/Inputs/Text.vue
@@ -62,7 +62,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#port {
-  max-width: 100%;
-}
 </style>
