312c56b46ddeec391455b8a91eee77b3e750a1e2
b-m-f <max@ehlers.berlin>
2020-11-22 12:38:17 +0100

    Updates to version 1.5.0

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -31,7 +31,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: { alerts },
   state: {
-    version: "1.4.5",
+    version: "1.5.0",
     keys: undefined,
     server: {
       ip: { v4: [undefined, undefined, undefined, undefined], v6: "" },
