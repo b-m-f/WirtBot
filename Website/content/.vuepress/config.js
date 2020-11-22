ad778101222b1fd2022964486e80f7508da1953d
b-m-f <max@ehlers.berlin>
2020-11-22 15:05:27 +0100

    feature(website): adds announcement for 1.5

--- Website/content/.vuepress/config.js
+++ Website/content/.vuepress/config.js
@@ -23,8 +23,9 @@ module.exports = {
           ],
           "/announcements/": [
             ["8-11-20", "8.11.20: New name for installer binary"],
-            ["31-10-20", "31.10.20: All aboard the container"],
             ["21-10-20", "21.10.20: Please update your installer"],
+            ["31-10-20", "31.10.20: All aboard the container"],
+            ["22-11-20", "22.11.20: Installer change and major Interface + System-Tests refactor"],
           ],
           "/developer-documentation/": [
             ["", "WirtBot developer documentation"],
