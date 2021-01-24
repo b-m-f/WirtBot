6f4c874a396c56496f94e588c5cafe8223c27c82
b-m-f <max@ehlers.berlin>
2021-01-24 16:43:27 +0100

    Updates to 1.5.8

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -31,7 +31,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: { alerts },
   state: {
-    version: "1.5.7",
+    version: "1.5.8",
     keys: undefined,
     server: {
       ip: { v4: [undefined, undefined, undefined, undefined], v6: "" },
