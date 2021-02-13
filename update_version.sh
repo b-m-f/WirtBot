cbc1d7f77cb7a70e85f44331e08aec6dcfad52ff
b-m-f <max@ehlers.berlin>
2021-02-13 11:23:36 +0100

    Removes installer

--- update_version.sh
+++ update_version.sh
@@ -6,6 +6,5 @@ while read line; do
 	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|version: \".*\",|version: \"$line\",|" {} \;
 	find . -type f -name "Cargo.toml" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
 	find . -type f -name "Cargo.lock" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
-	find Installer -type f -name "WirtBot.docker-compose.yml.j2" -not -path "*/node_modules**" -exec sed -i -e "s|bmff/wirtbot:.*|bmff/wirtbot:$line|" {} \;
-	echo "Finished updating please release the installer if necessary and create a Tag to release"
+	echo "Finished updating, now create a Tag to release"
 done < $filename
