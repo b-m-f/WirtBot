e2766e6e80ccd02bda11766d098ffbb42a021beb
b-m-f <max@ehlers.berlin>
2021-02-14 08:32:57 +0100

    feat(builds): adds multi arch docker builds via github actions

--- update_version.sh
+++ update_version.sh
@@ -5,6 +5,8 @@ while read line; do
 	find . -type f -name "package.json" -not -path "*/node_modules**" -exec sed -i -e "s|version\": \".*\"|version\": \"$line\"|" {} \;
 	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|version: \".*\",|version: \"$line\",|" {} \;
 	find . -type f -name "Cargo.toml" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
-	find . -type f -name "Cargo.lock" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
 	echo "Finished updating, now create a Tag to release"
 done < $filename
+cd Core
+cargo build
+cd ..
