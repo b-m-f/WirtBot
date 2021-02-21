e25fd0e06ddaa7899b9c7016ba9b3a6e645b5f10
b-m-f <max@ehlers.berlin>
2021-02-21 23:13:44 +0100

    Updates to 2.3.1

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -30,7 +30,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: { alerts },
   state: {
-    version: "2.3.0",
+    version: "2.3.1",
     keys: { public: undefined, private: undefined },
     server: {
       ip: { v4: [undefined, undefined, undefined, undefined], v6: "" },
