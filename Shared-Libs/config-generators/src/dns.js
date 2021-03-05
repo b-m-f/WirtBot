43ba63d7162001af230f20745dfe97a97f7d4acf
b-m-f <max@ehlers.berlin>
2021-03-05 11:19:22 +0100

    fix(Shared-Libs): fix dns config generation and the corresponding tests

--- Shared-Libs/config-generators/src/dns.js
+++ Shared-Libs/config-generators/src/dns.js
@@ -1,56 +1,81 @@
+function expandIPv6(ip) {
+  const parts = ip.split(":");
+  const potentialExpansionIndex = parts.findIndex((part) => part === "");
+  const containsEmptyString = potentialExpansionIndex > -1;
+
+  if (containsEmptyString) {
+    if (parts[parts.length - 1] === "") {
+      parts.pop();
+    }
+    parts.splice(potentialExpansionIndex, 1);
+    const amountToBeInserted = 7 - parts.length;
+    const newParts = [...Array(amountToBeInserted)].map(() => "0000");
+    parts.splice(potentialExpansionIndex, 0, ...newParts);
+  }
+
+  return parts.join(":");
+}
+
 // This function creates an RFC 1035 DNS master file
 export function generateDNSFile(server, clients, network) {
-    const tls = network.dns.tls;
-    const tlsName = network.dns.tlsName;
-    const dnsV4 = network.dns.ip.v4 || "1.1.1.1";
-    const dnsV6 = network.dns.ip.v6;
-    const deviceNames = clients.map(client => {
-        client.name = client.name.split(' ').join('-');
-        if (!server.name) {
-            server.name = "wirtbot";
-        }
+  const tls = network.dns.tls;
+  const tlsName = network.dns.tlsName;
+  const dnsV4 = network.dns.ip.v4 || "1.1.1.1";
+  const dnsV6 = network.dns.ip.v6;
 
-        if (client.ip.v6 && client.ip.v4) {
-            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dns.name}
-        ${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dns.name}`
-        }
-        if (client.ip.v6 && !client.ip.v4) {
-            return `${server.subnet.v6 + client.ip.v6} ${client.name}.${network.dns.name}`
-        }
-        if (!client.ip.v6 && client.ip.v4) {
-            return `${server.subnet.v4 + client.ip.v4} ${client.name}.${network.dns.name}`
-        }
-    })
-    const serverName = () => {
-        if (server.subnet.v6 && server.subnet.v4) {
-            return `${server.subnet.v4 + '1'} ${server.name}.${network.dns.name}
-        ${server.subnet.v6 + '1'} ${server.name}.${network.dns.name}`
-        }
-        if (server.subnet.v6 && !server.subnet.v4) {
-            return `${server.subnet.v6 + "1"} ${server.name}.${network.dns.name}`
-        }
-        if (!server.subnet.v6 && server.subnet.v4) {
-            return `${server.subnet.v4 + "1"} ${server.name}.${network.dns.name}`
-        }
+  // expand IPv6 subnet
+  if (server.subnet.v6) {
+    server.subnet.v6 = expandIPv6(server.subnet.v6);
+  }
+
+  const deviceNames = clients.map((client) => {
+    client.name = client.name.split(" ").join("-");
+    if (!server.name) {
+      server.name = "wirtbot";
     }
 
-    const forwardConfig = () => {
-        if (tls) {
-            return `forward . ${dnsV4 ? `tls://` + dnsV4 + " " : ""}${dnsV6 ? `tls://` + dnsV6 + "" : ""}{
+    if (client.ip.v6 && client.ip.v4) {
+      return `${server.subnet.v4}.${client.ip.v4} ${client.name}.${network.dns.name}
+        ${server.subnet.v6}:${client.ip.v6} ${client.name}.${network.dns.name}`;
+    }
+    if (client.ip.v6 && !client.ip.v4) {
+      return `${server.subnet.v6}:${client.ip.v6} ${client.name}.${network.dns.name}`;
+    }
+    if (!client.ip.v6 && client.ip.v4) {
+      return `${server.subnet.v4}.${client.ip.v4} ${client.name}.${network.dns.name}`;
+    }
+  });
+  const serverName = () => {
+    if (server.subnet.v6 && server.subnet.v4) {
+      return `${server.subnet.v4}.1 ${server.name}.${network.dns.name}
+        ${server.subnet.v6}:1 ${server.name}.${network.dns.name}`;
+    }
+    if (server.subnet.v6 && !server.subnet.v4) {
+      return `${server.subnet.v6}:1 ${server.name}.${network.dns.name}`;
+    }
+    if (!server.subnet.v6 && server.subnet.v4) {
+      return `${server.subnet.v4}.1 ${server.name}.${network.dns.name}`;
+    }
+  };
+
+  const forwardConfig = () => {
+    if (tls) {
+      return `forward . ${dnsV4 ? `tls://` + dnsV4 + " " : ""}${
+        dnsV6 ? `tls://` + dnsV6 + "" : ""
+      }{
        except ${network.dns.name} lan local home fritz.box
        tls_servername ${tlsName}
        health_check 5s
-    }`
-        } else {
-            return `forward . ${dnsV4 ? dnsV4 + "" : ""}${dnsV6 ? dnsV6 : ""} {
+    }`;
+    } else {
+      return `forward . ${dnsV4 ? dnsV4 + "" : ""}${dnsV6 ? dnsV6 : ""} {
        except ${network.dns.name} lan local home fritz.box
        health_check 5s
-    }`
-        }
+    }`;
     }
+  };
 
-
-    const masterFile = `. {
+  const masterFile = `. {
     reload
     ${forwardConfig()}
     cache 30
@@ -60,6 +85,6 @@ ${network.dns.name} {
         ${serverName()}
         ${deviceNames.join("\n        ")}
     }
-}`
-    return masterFile.trim();
+}`;
+  return masterFile.trim();
 }
