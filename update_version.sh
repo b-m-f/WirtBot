af0bb43ebc2266302faa9cb245009834caa6b205
b-m-f <max@ehlers.berlin>
2021-03-06 10:42:33 +0100

    fix(project): fix automatic version updating of store version

--- update_version.sh
+++ update_version.sh
@@ -3,10 +3,10 @@ filename='.version'
 while read line; do
 	echo "Updating all packages to $line"
 	find . -type f -name "package.json" -not -path "*/node_modules**" -exec sed -i -e "s|version\": \".*\"|version\": \"$line\"|" {} \;
-	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|version: \".*\",|version: \"$line\",|" {} \;
+	find . -type f -wholename "**store/index.js" -not -path "*/node_modules**" -exec sed -i -e "s|const version = \".*\"|const version = \"$line\"|" {} \;
 	find . -type f -name "Cargo.toml" -not -path "*/node_modules**" -exec sed -i -e "s|^version = \".*\"|version = \"$line\"|" {} \;
-	echo "Finished updating, now create a Tag to release"
 done < $filename
 cd Core
 cargo build
 cd ..
+echo "Finished updating, now create a Tag to release"
