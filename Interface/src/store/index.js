bd8cb2af16daf9c0f97d7e53b8572a1f95aeb39d
b-m-f <max@ehlers.berlin>
2021-03-30 20:54:14 +0200

    Updates to 2.6.3

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -39,7 +39,7 @@ async function addConfigToDevice(newDevice, server) {
   });
 }
 
-const version = "2.6.2";
+const version = "2.6.3";
 
 const versionFromAppPlugin = (store) => {
   store.subscribe((mutation, state) => {
