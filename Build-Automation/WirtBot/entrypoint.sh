4ef276713b1884d412c75415bc9d4ae744b2e399
b-m-f <max@ehlers.berlin>
2021-03-31 21:10:14 +0200

    fix(Build, Website): Various small fixes and comments

--- Build-Automation/WirtBot/entrypoint.sh
+++ Build-Automation/WirtBot/entrypoint.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
-set -eou pipefail
+
+# Start wireguard interface
+wg-quick up server
+
+# Start firewall for masquerading traffic when routing traffic from clients in the network
 /firewall.sh
+
+# Start all services needed for WirtBot wit s6-overlay supervision
 /init
