e32a4f019a320cc6997d039b299df0447736338c
b-m-f <max@ehlers.berlin>
2021-05-02 14:56:37 +0200

    chore: add convenience-script to automatically update dependencies

---
+++ convenience-scripts/update-dependencies.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if ! command -v ncu &> /dev/null
+then
+    echo "Please install npm-check-updates with 'npm i -g npm-check-updates'"
+    exit
+fi
+# TODO: remove the reject once the sass-loader 11 is supported by vue-cli
+cd ./Website && ncu -u --reject sass-loader && npm i && cd - &&
+cd ./Interface && ncu -u --reject sass-loader && npm i && cd - &&
+cd ./System-Tests && ncu -u && npm i && cd - &&
+cd ./Interface/src/lib/crate && cargo update && cd - &&
+cd ./Core && cargo update && cd -
+
