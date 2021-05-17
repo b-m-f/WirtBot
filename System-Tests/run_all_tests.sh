3f88cc9236e57773dd7e37a5e4e2408acf4329b9
b-m-f <max@ehlers.berlin>
2021-05-17 16:54:08 +0200

    fix: correctly capture test env variables

--- System-Tests/run_all_tests.sh
+++ System-Tests/run_all_tests.sh
@@ -5,6 +5,10 @@ set -ex
 cd tests/
 for test in test_*
 do
-  URL=localhost:8080 API=localhost:3030 node tests.mjs $test
+  # capture external ENV variables if present or set project defaults
+  URL=$URL || localhost:8080
+  API=$API || localhost:3030
+  
+  URL=$URL API=$API node tests.mjs $test
 done
 cd -
