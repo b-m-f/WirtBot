1c9af28bbd5c805181aaa9983e3c53e7514e2306
b-m-f <max@ehlers.berlin>
2021-05-23 20:11:38 +0200

    Updates to 2.7.8

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -39,7 +39,7 @@ async function addConfigToDevice(newDevice, server) {
   });
 }
 
-const version = "2.7.7";
+const version = "2.7.8";
 
 const versionFromAppPlugin = (store) => {
   store.subscribe((mutation, state) => {
