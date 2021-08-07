8b69ab29de8e580bb87f35752763954d356b7a62
b-m-f <max@ehlers.berlin>
2021-08-07 21:26:34 +0200

    Updates to 3.0.1

--- Interface/src/store/index.js
+++ Interface/src/store/index.js
@@ -39,7 +39,7 @@ async function addConfigToDevice(newDevice, server) {
   });
 }
 
-const version = "3.0.0";
+const version = "3.0.1";
 
 const versionFromAppPlugin = (store) => {
   store.subscribe((mutation, state) => {
