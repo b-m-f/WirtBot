790cc90d2a0cb7dd7b5d23a24570d2b2f50dc51c
b-m-f <max@ehlers.berlin>
2022-04-05 17:52:42 +0200

    fix: Updates main app components to work with new Vue3 style

--- Interface/src/i18n.js
+++ Interface/src/i18n.js
@@ -1,7 +1,5 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import {createI18n} from 'vue-i18n';
 
-Vue.use(VueI18n)
 
 function loadLocaleMessages () {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
@@ -10,14 +8,18 @@ function loadLocaleMessages () {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key)
+      messages[locale] = locales(key).default
     }
   })
+  for (let key in messages.en) {
+  console.log(key)} 
   return messages
+
 }
 
-export default new VueI18n({
+export default createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  allowComposition: true,
   messages: loadLocaleMessages()
 })
--- Interface/src/main.js
+++ Interface/src/main.js
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import {createApp,h} from "vue";
 import App from "./App.vue";
 
 import router from "./routes";
@@ -6,13 +6,20 @@ import store from "./store";
 import i18n from "./i18n";
 import icons from "./icons";
 
-Vue.component("font-awesome-icon", icons);
-
-Vue.config.productionTip = false;
-
-new Vue({
+const app = createApp({
   router,
   store,
   i18n,
-  render: h => h(App)
-}).$mount("#app");
+  render: () => h(App)
+})
+
+app.component("font-awesome-icon", icons);
+
+app.config.productionTip = false;
+
+app.use(router);
+app.use(store);
+app.use(i18n);
+app.use(icons);
+
+app.mount("#app");
