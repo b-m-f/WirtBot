7aae101cec4d72de09fe3c8ef907f2386736f97b
b-m-f <max@ehlers.berlin>
2022-04-06 20:05:26 +0200

    chore: Upgrades all dependencies

--- convenience-scripts/update-dependencies.sh
+++ convenience-scripts/update-dependencies.sh
@@ -5,10 +5,11 @@ then
     echo "Please install npm-check-updates with 'npm i -g npm-check-updates'"
     exit
 fi
-# TODO: remove the reject once the sass-loader 11 is supported by vue-cli
-cd ./Website && ncu -u --reject sass-loader && npm i && npm audit fix && cd - &&
-cd ./Interface && ncu -u --reject sass-loader && npm i && npm audit fix && cd - &&
-cd ./System-Tests && ncu -u && npm i && npm audit fix && cd - &&
-cd ./Interface/src/lib/crate && cargo update && cd - &&
-cd ./Core && cargo update && cd -
+# TODO: remove the reject once the sass-loader 11 is supported by vuepress, which means it needs to support webpack5 properly.
+# Wait for v2
+cd ./Website && ncu -u --reject sass-loader,webpack && npm i && npm audit fix || cd -
+cd ./Interface && ncu -u && npm i && npm audit fix || cd -
+cd ./System-Tests && ncu -u && npm i && npm audit fix || cd -
+cd ./Interface/src/lib/crate && cargo update || cd -
+cd ./Core && cargo update || cd -
 
