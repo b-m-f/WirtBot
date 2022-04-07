283a23b4a366d85d60900e18e689ae7b5550caaf
b-m-f <max@ehlers.berlin>
2022-04-07 12:34:09 +0200

    Fixes website build

--- convenience-scripts/update-dependencies.sh
+++ convenience-scripts/update-dependencies.sh
@@ -7,7 +7,7 @@ then
 fi
 # TODO: remove the reject once the sass-loader 11 is supported by vuepress, which means it needs to support webpack5 properly.
 # Wait for v2
-cd ./Website && ncu -u --reject sass-loader,webpack && npm i && npm audit fix || cd -
+cd ./Website && ncu -u --reject sass-loader,webpack,node-sass && npm i && npm audit fix || cd -
 cd ./Interface && ncu -u && npm i && npm audit fix || cd -
 cd ./System-Tests && ncu -u && npm i && npm audit fix || cd -
 cd ./Interface/src/lib/crate && cargo update || cd -
