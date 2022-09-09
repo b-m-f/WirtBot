43901205ef178e73ac4ff0801eb24a4cd2781bdd
b-m-f <max@ehlers.berlin>
2022-09-09 16:14:51 +0200

    Adds ssl distinction between interface and core

--- Build-Automation/WirtBot/entrypoint.sh
+++ Build-Automation/WirtBot/entrypoint.sh
@@ -2,6 +2,22 @@
 
 set -eou pipefail
 
+if [[ -z "${SSL_PEM_CERT_INTERFACE}" ]]; then
+# Env variable is not defined
+## || true is added so it does not crash when the config file doesnt exist yet
+rm /etc/nginx/nginx.conf || true
+mv /etc/nginx/nginx.http /etc/nginx/nginx.conf
+else
+if [[ -z "${SSL_KEY_INTERFACE}" ]]; then
+else
+# Env variable is defined
+echo "Preparing NGINX for SSL"
+rm /etc/nginx/nginx.conf || true
+mv /etc/nginx/nginx.https /etc/nginx/nginx.conf
+mv $SSL_PEM_CERT_INTERFACE /interface/public_key
+mv $SSL_KEY_INTERFACE /interface/private_key
+fi
+
 # Start wireguard interface
 wg-quick up server
 
