848b44df9438d05810849a46f28f438a9fd3de68
b-m-f <max@ehlers.berlin>
2022-09-12 12:20:56 +0200

    [Feature] wg synnconf has lead to the server loosing the connections. A complete restart of the interface is more reliable and therefore chosen as the approach for now

--- Core/wireguard-restarter.sh
+++ Core/wireguard-restarter.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-while inotifywait -e close_write /etc/wireguard/server.conf; do wg syncconf server <(wg-quick strip server); done
+while inotifywait -e close_write /etc/wireguard/server.conf; do
+wg-quick down server;
+wg-quick up server; 
+echo "WireGuard interface has been restartd";
+echo "Old Peers were removed";
+echo "New Peers were added";
+done
